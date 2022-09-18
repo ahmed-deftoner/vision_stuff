ab3537c117824324d22a8487b1834fe156f8b8c2
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-09-18 14:47:39 +0500

    image chooser

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from asyncio.windows_events import NULL
+import io
+import os
 import PySimpleGUI as sg
 import cv2
 import numpy as np
+from PIL import Image
+
+file_types = [("JPEG (*.jpg)", "*.jpg"),
+              ("All files (*.*)", "*.*")]
 
 def main():
     sg.theme("LightGreen")
@@ -8,7 +15,13 @@ def main():
     # Define the window layout
     layout = [
         [sg.Text("OpenCV Demo", size=(1, 1), justification="center")],
-        [sg.Image(filename="", key="-IMAGE-",size=(4,4))],
+        [sg.Image(key="-IMAGE-")],
+        [
+            sg.Text("Image File"),
+            sg.Input(size=(10, 1), key="-FILE-"),
+            sg.FileBrowse(file_types=file_types),
+            sg.Button("Load Image"),
+        ],
         [sg.Radio("None", "Radio", True, size=(5, 1))],
         [
             sg.Radio("enhance", "Radio", size=(5, 1), key="-ENHANCE-"),
@@ -27,24 +40,31 @@ def main():
     # Create the window and show it without the plot
     window = sg.Window("OpenCV Integration", layout, location=(200, 200),size=(600,600))
 
-    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture(0)
 
     while True:
         event, values = window.read(timeout=20)
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
-        ret, frame = cap.read()
-
-        if values["-ENHANCE-"]:
+        #ret, frame = cap.read()
+        image = NULL
+        if values["-ENHANCE-"] & image != NULL:
             enh_val = values["-ENHANCE SLIDER-"] / 40
             clahe = cv2.createCLAHE(clipLimit=enh_val, tileGridSize=(8, 8))
-            lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+            lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
             lab[:, :, 0] = clahe.apply(lab[:, :, 0])
-            frame = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
-
-        imgbytes = cv2.imencode(".png", frame)[1].tobytes()
-        window["-IMAGE-"].update(data=imgbytes)
+            image = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+        if event == "Load Image":
+            filename = values["-FILE-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE-"])
+                image.thumbnail((400, 400))
+                bio = io.BytesIO()
+                image.save(bio, format="PNG")
+                window["-IMAGE-"].update(data=bio.getvalue())
+            #imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+            #window["-IMAGE-"].update(data=imgbytes)
 
     window.close()
 
