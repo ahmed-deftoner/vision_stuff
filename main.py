ceb677220f5d2fd5102069bd3b36079978dfedd3
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-09-18 13:12:49 +0500

    opencv gui

--- main.py
+++ main.py
@@ -1,16 +1,118 @@
 import PySimpleGUI as sg
+import cv2
+import numpy as np
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+def main():
+    sg.theme("LightGreen")
 
-# Create the window
-window = sg.Window("Demo", layout)
+    # Define the window layout
+    layout = [
+        [sg.Text("OpenCV Demo", size=(20, 1), justification="center")],
+        [sg.Image(filename="", key="-IMAGE-")],
+        [sg.Radio("None", "Radio", True, size=(5, 1))],
+        [
+            sg.Radio("threshold", "Radio", size=(5, 1), key="-THRESH-"),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(20, 15),
+                key="-THRESH SLIDER-",
+            ),
+        ],
+        [
+            sg.Radio("canny", "Radio", size=(5, 1), key="-CANNY-"),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(10, 15),
+                key="-CANNY SLIDER A-",
+            ),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(10, 15),
+                key="-CANNY SLIDER B-",
+            ),
+        ],
+        [
+            sg.Radio("blur", "Radio", size=(5, 1), key="-BLUR-"),
+            sg.Slider(
+                (1, 11),
+                1,
+                1,
+                orientation="h",
+                size=(20, 15),
+                key="-BLUR SLIDER-",
+            ),
+        ],
+        [
+            sg.Radio("hue", "Radio", size=(5, 1), key="-HUE-"),
+            sg.Slider(
+                (0, 225),
+                0,
+                1,
+                orientation="h",
+                size=(20, 15),
+                key="-HUE SLIDER-",
+            ),
+        ],
+         [
+            sg.Radio("enhance", "Radio", size=(5, 1), key="-ENHANCE-"),
+            sg.Slider(
+                (1, 255),
+                128,
+                1,
+                orientation="h",
+                size=(20, 15),
+                key="-ENHANCE SLIDER-",
+            ),
+        ],
+        [sg.Button("Exit", size=(5, 1))],
+    ]
 
-# Create an event loop
-while True:
-    event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
-        break
+    # Create the window and show it without the plot
+    window = sg.Window("OpenCV Integration", layout, location=(200, 200))
 
-window.close()
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        event, values = window.read(timeout=20)
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+        ret, frame = cap.read()
+
+        if values["-THRESH-"]:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 0]
+            frame = cv2.threshold(
+                frame, values["-THRESH SLIDER-"], 255, cv2.THRESH_BINARY
+            )[1]
+        elif values["-CANNY-"]:
+            frame = cv2.Canny(
+                frame, values["-CANNY SLIDER A-"], values["-CANNY SLIDER B-"]
+            )
+        elif values["-BLUR-"]:
+            frame = cv2.GaussianBlur(frame, (21, 21), values["-BLUR SLIDER-"])
+        elif values["-HUE-"]:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            frame[:, :, 0] += int(values["-HUE SLIDER-"])
+            frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+        elif values["-ENHANCE-"]:
+            enh_val = values["-ENHANCE SLIDER-"] / 40
+            clahe = cv2.createCLAHE(clipLimit=enh_val, tileGridSize=(8, 8))
+            lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+            lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+            frame = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+        imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+        window["-IMAGE-"].update(data=imgbytes)
+
+    window.close()
+
+main()
