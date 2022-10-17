a9c8ed0785d36d134eac24d8a3befed3fa79b543
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-17 23:40:33 +0500

    plt show

--- convolution.py
+++ convolution.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 
 def plot_image(img: np.array):
     plt.figure(figsize=(6, 6))
-    plt.imshow(img, cmap='gray');
+    plt.imshow(img, cmap='gray')
+    plt.show()
 
 def plot_two_images(img1: np.array, img2: np.array):
     _, ax = plt.subplots(1, 2, figsize=(12, 6))
     ax[0].imshow(img1, cmap='gray')
-    ax[1].imshow(img2, cmap='gray');
-    
+    ax[1].imshow(img2, cmap='gray')
+    plt.show()
+
 def input_filter(matrix):
     R = int(input("Enter the dimension:"))
     print("Enter the entries rowwise:")
@@ -31,5 +33,9 @@ def main():
     matrix = []
     n = input_filter(matrix)
     print_filter(matrix, n)
+    img = Image.open('cloud.png')
+    img = ImageOps.grayscale(img)
+    img = img.resize(size=(224, 224))
+    plot_image(img=img)
 
 main()
