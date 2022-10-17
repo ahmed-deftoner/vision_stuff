14545e449234fbcd0455e9a232bd08e9087c7596
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-17 23:33:24 +0500

    refactor

--- convolution.py
+++ convolution.py
@@ -1,15 +1,23 @@
-def main():
+
+def input_filter(matrix):
     R = int(input("Enter the dimension:"))
-    matrix = []
     print("Enter the entries rowwise:")
-    for i in range(R):          # A for loop for row entries
-        a =[]
-        for j in range(R):      # A for loop for column entries
+    for i in range(R):         
+        a = []
+        for j in range(R):      
             a.append(int(input()))
         matrix.append(a)
-    for i in range(R):
-        for j in range(R):
+    return R
+
+def print_filter(matrix, n):
+    for i in range(n):
+        for j in range(n):
             print(matrix[i][j], end = " ")
         print()
+
+def main():
+    matrix = []
+    n = input_filter(matrix)
+    print_filter(matrix, n)
 
 main()
