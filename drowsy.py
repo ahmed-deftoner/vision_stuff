9736d57b1cf7d6978e00a5dfcf5e5b95188f488a
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-28 18:41:24 +0500

    distance funcs

--- drowsy.py
+++ drowsy.py
@@ -4,3 +4,102 @@ import time
 import numpy as np
 import mediapipe as mp
 from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates as denormalize_coordinates
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+def distance(point_1, point_2):
+    ...
+    ...
+    return dist
+ 
+def get_ear(landmarks, refer_idxs, frame_width, frame_height):
+    ...
+    ...
+    return ear, coords_points
+ 
+def calculate_avg_ear(landmarks, left_eye_idxs, right_eye_idxs, image_w, image_h):
+    ...
+    ...
+    return Avg_EAR, (left_lm_coordinates, right_lm_coordinates)
+ 
+def get_mediapipe_app(
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+):
+    """Initialize and return Mediapipe FaceMesh Solution Graph object"""
+    face_mesh = mp.solutions.face_mesh.FaceMesh(
+        max_num_faces=max_num_faces,
+        refine_landmarks=refine_landmarks,
+        min_detection_confidence=min_detection_confidence,
+        min_tracking_confidence=min_tracking_confidence,
+    )
+ 
+    return face_mesh
+ 
+def plot_eye_landmarks(frame, left_lm_coordinates, 
+                       right_lm_coordinates, color
+                       ):
+    for lm_coordinates in [left_lm_coordinates, right_lm_coordinates]:
+        if lm_coordinates:
+            for coord in lm_coordinates:
+                cv2.circle(frame, coord, 2, color, -1)
+ 
+    frame = cv2.flip(frame, 1)
+    return frame
+ 
+ 
+def plot_text(image, text, origin, 
+              color, font=cv2.FONT_HERSHEY_SIMPLEX, 
+              fntScale=0.8, thickness=2
+              ):
+    image = cv2.putText(image, text, origin, font, fntScale, color, thickness)
+    return image
