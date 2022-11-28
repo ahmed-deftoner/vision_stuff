bca62c174f02207da621cc22036a1c0b401f0061
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-28 18:39:22 +0500

    update imports

--- drowsy.py
+++ drowsy.py
@@ -1,26 +1,6 @@
+
 import cv2
+import time
 import numpy as np
-import matplotlib.pyplot as plt
 import mediapipe as mp
- 
-mp_facemesh = mp.solutions.face_mesh
-mp_drawing  = mp.solutions.drawing_utils
-denormalize_coordinates = mp_drawing._normalized_to_pixel_coordinates
- 
- 
-# Landmark points corresponding to left eye
-all_left_eye_idxs = list(mp_facemesh.FACEMESH_LEFT_EYE)
-# flatten and remove duplicates
-all_left_eye_idxs = set(np.ravel(all_left_eye_idxs)) 
- 
-# Landmark points corresponding to right eye
-all_right_eye_idxs = list(mp_facemesh.FACEMESH_RIGHT_EYE)
-all_right_eye_idxs = set(np.ravel(all_right_eye_idxs))
- 
-# Combined for plotting - Landmark points for both eye
-all_idxs = all_left_eye_idxs.union(all_right_eye_idxs)
- 
-# The chosen 12 points:   P1,  P2,  P3,  P4,  P5,  P6
-chosen_left_eye_idxs  = [362, 385, 387, 263, 373, 380]
-chosen_right_eye_idxs = [33,  160, 158, 133, 153, 144]
-all_chosen_idxs = chosen_left_eye_idxs + chosen_right_eye_idxs
+from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates as denormalize_coordinates
