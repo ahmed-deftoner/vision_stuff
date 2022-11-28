58b78f7e709475b2d60c9dc736dbb8cfc7767526
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-28 18:42:41 +0500

    ear distance

--- drowsy.py
+++ drowsy.py
@@ -5,58 +5,8 @@ import numpy as np
 import mediapipe as mp
 from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates as denormalize_coordinates
 
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
 def distance(point_1, point_2):
-    ...
-    ...
+    dist = sum([(i - j) ** 2 for i, j in zip(point_1, point_2)]) ** 0.5
     return dist
  
 def get_ear(landmarks, refer_idxs, frame_width, frame_height):
